0a67184cdec169e94f8077e1eedda1e246f095be
bakuzan <imannoyed4days@hotmail.co.uk>
2025-09-14 16:29:05 +0100

    Changing the colours of tiles

--- src/components/TileRegistry.cpp
+++ src/components/TileRegistry.cpp
@@ -19,14 +19,14 @@ std::unordered_map<char, TileDefinition> TileRegistry::createTileRegistry()
 {
     std::unordered_map<char, TileDefinition> registry;
     // TODO Update to reflect the real tiles using sprites
-    registry['#'] = {sf::Color::Magenta,
+    registry['#'] = {sf::Color(165, 42, 42),
                      TileProperties::createTileProperties(TileType::SOLID, Solidity::BOTH, false, false)};
-    registry['.'] = {sf::Color(165, 42, 42),
+    registry['.'] = {sf::Color::Black,
                      TileProperties::createTileProperties(TileType::EMPTY, Solidity::NONE, false, false)};
 
-    registry['-'] = {sf::Color(35, 145, 9),
+    registry['-'] = {sf::Color(66, 66, 66),
                      TileProperties::createTileProperties(TileType::PLATFORM, Solidity::TOP, false, false)};
-    registry['='] = {sf::Color::Green,
+    registry['='] = {sf::Color(134, 134, 134),
                      TileProperties::createTileProperties(TileType::BACKGROUND, Solidity::NONE, false, false)};
 
     registry['~'] = {sf::Color::Blue,

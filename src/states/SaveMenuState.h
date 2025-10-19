bebc047f37e2d35ea60f96894fb4bed5e548bfb7
bakuzan <imannoyed4days@hotmail.co.uk>
2025-10-19 16:53:33 +0100

    Working SaveMenuState in terms of user input, just no actual saving!

--- src/states/SaveMenuState.h
+++ src/states/SaveMenuState.h
@@ -3,10 +3,13 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <array>
+
 #include "ui/Button.h"
 #include "core/GameData.h"
 #include "core/State.h"
 #include "core/StateManager.h"
+#include "data/SaveSlot.h"
 
 class SaveMenuState : public State
 {
@@ -19,8 +22,29 @@ private:
     sf::RectangleShape background;
     sf::Text saveText;
 
+    // Save slots
+    std::array<SaveSlot, 3> slots;
+    int highlightedSlot = 0;
+
+    // Confirmation overlay
+    bool showingConfirm = false;
+    sf::RectangleShape confirmBox;
+    sf::Text confirmText;
+    std::array<sf::Text, 2> confirmButtons; // [0] = Yes, [1] = No
+    int highlightedConfirm = 1;             // default = No
+
 private:
     void updateMenuItemPositions();
+
+    void refreshSlots();
+    void moveHighlight(int dir);
+    void selectSlot();
+    void showConfirmation();
+    void handleConfirmInput(int dir);
+    void confirmSelection();
+
+    int hitTestConfirm(sf::Vector2f mousePos);
+    int hitTestSlots(sf::Vector2f mousePos);
 
 public:
     SaveMenuState(GameData &data, StateManager &manager, sf::RenderWindow &window);

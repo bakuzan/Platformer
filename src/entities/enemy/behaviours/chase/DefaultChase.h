285ef71b1321d63fa98a786ee2eaeb63f8f13a4c
bakuzan <imannoyed4days@hotmail.co.uk>
2026-05-30 18:42:13 +0100

    Guard normalisation

--- src/entities/enemy/behaviours/chase/DefaultChase.h
+++ src/entities/enemy/behaviours/chase/DefaultChase.h
@@ -14,7 +14,14 @@ public:
 
         sf::Vector2f dir = playerPosition - e.getPosition();
         float len = std::sqrt(dir.x * dir.x + dir.y * dir.y);
-        dir /= len;
+        if (len > 0.f)
+        {
+            dir /= len;
+        }
+        else
+        {
+            dir = {0.f, 0.f};
+        }
 
         e.setVelocity(dir);
     }
